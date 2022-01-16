0f2ba07ad1909bca3f04156e5bd83fdecd10a18e
ritazhan <zhan.rita.info@gmail.com>
2022-01-16 18:09:32 -0500

    Started on moving div with cursor. Editing graphic page.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+$(function() {
+  console.log( "ready!" );
+});
+
 $(".landingPage-nav-link").hover(function(){
-  var linkLabel = $(this).attr('data-label');
+  let linkLabel = $(this).attr('data-label');
   $('.landingPage-nav-text').html(linkLabel);
 });
+
+$(".landingPage-nav-link").click(function(){
+  console.log('link click function works');
+})
+
+$(".logo").hover(function(){
+  $(document).mousemove(function(e){
+  $(".cursor").css({left:e.pageX, top:e.pageY});
+  console.log('cursor function working')
+})
+});
+
+// $(document).mousemove(function(e){
+//  $(".cursor").css({left:e.pageX, top:e.pageY});
+//})
