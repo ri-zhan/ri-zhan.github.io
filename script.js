0b64b87df0d9dd7537ebef4a1962dc7b40a2e7b2
ritazhan <zhan.rita.info@gmail.com>
2022-01-17 13:12:19 -0500

    Editing graphic page

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 $(function() {
   console.log( "ready!" );
-});
 
-$(".landingPage-nav-link").hover(function(){
-  let linkLabel = $(this).attr('data-label');
-  $('.landingPage-nav-text').html(linkLabel);
-});
-
-$(".landingPage-nav-link").click(function(){
-  console.log('link click function works');
-})
-
-$(".logo").hover(function(){
-  $(document).mousemove(function(e){
+$('landingPage-nav').hover(function(e){
+  $(this).mousemove(function(e){
   $(".cursor").css({left:e.pageX, top:e.pageY});
   console.log('cursor function working')
 })
 });
+});
 
+// this following code alone works so it's just the 'landingPage-nav' part that seems to be malfunctioning?
 // $(document).mousemove(function(e){
 //  $(".cursor").css({left:e.pageX, top:e.pageY});
-//})
+// })
