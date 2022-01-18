366ea64c548ccdc5632eacc2892aa6fcbf264c08
ritazhan <zhan.rita.info@gmail.com>
2022-01-18 02:28:57 -0500

    Attempted more JS for looped automatic scrolling. Adding onto graphics page.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-$(function() {
-  console.log( "ready!" );
+console.log( "ready!" );
 
-$('landingPage-nav').hover(function(e){
-  $(this).mousemove(function(e){
-  $(".cursor").css({left:e.pageX, top:e.pageY});
-  console.log('cursor function working')
-})
+$('#landingPage-nav').mousemove(function(e){
+  $(".cursor").css({left: e.pageX, top:e.pageY});
 });
+
+$('#landingPage-nav').mouseleave(function(){
+  $(".cursor").css({left: '50%', top:'50%', transform: 'translate(-50%, -50%)'});
 });
 
 // this following code alone works so it's just the 'landingPage-nav' part that seems to be malfunctioning?
