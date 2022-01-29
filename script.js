3fc2ff556a4372dc7a5592e5f41764adfcf1e194
ritazhan <zhan.rita.info@gmail.com>
2022-01-29 00:36:10 -0500

    Added cursor functions to graphic and graphic-artchitecture. Also fixed margins of graphic-architecture.

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ $('#landingPage-nav').mousemove(function(e){
 });
 
 $('#landingPage-nav').mousemove(function(e){
-  $("#cursor-back").css({left: e.pageX, top:e.pageY});
+  $("#landingPage-diamond").css({left: e.pageX, top:e.pageY});
 });
 
 $('#landingPage-nav').mouseleave(function(){
   $(".cursor").css({left: '50%', top:'50%', transform: 'translate(-50%, -50%)'});
 });
+
+$('#projectPage-nav').mousemove(function(e){
+  $("#cursor-overlay").css({left: e.pageX, top:e.pageY});
+});
+
 
 // this following code alone works so it's just the 'landingPage-nav' part that seems to be malfunctioning?
 // $(document).mousemove(function(e){
