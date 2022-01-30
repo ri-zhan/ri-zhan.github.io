f182d2253e6c4e339dc34760649047257b80cad7
ritazhan <zhan.rita.info@gmail.com>
2022-01-29 20:09:30 -0500

    Added slides to graphic-architecture

--- script.js
+++ script.js
@@ -17,7 +17,23 @@ $('#projectPage-nav').mousemove(function(e){
 });
 
 
-// this following code alone works so it's just the 'landingPage-nav' part that seems to be malfunctioning?
-// $(document).mousemove(function(e){
-//  $(".cursor").css({left:e.pageX, top:e.pageY});
-// })
+let slideIndex = 1;
+showSlides(slideIndex)
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  if  (n > $('.slides').length) {slideIndex = 1}
+  if (n < 1) {slideIndex = $('.slides').length}
+  for (i = 0; i < $('.slides').length; i++){
+    $('.slides')[i].style.display = "none";
+  }
+  $('.slides')[slideIndex-1].style.display = "block";
+}
