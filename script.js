db661a3ccd4cf8b82ce9302cd712d16bc0d1f572
ritazhan <zhan.rita.info@gmail.com>
2022-02-01 00:13:20 -0500

    Working on JS loop on graphic.html and graphic-architecture

--- script.js
+++ script.js
@@ -17,23 +17,101 @@ $('#projectPage-nav').mousemove(function(e){
 });
 
 
-let slideIndex = 1;
-showSlides(slideIndex)
+var doc = window.document,
+  context = doc.querySelector('.js-loop'),
+  clones = context.querySelectorAll('.is-clone'),
+  disableScroll = false,
+  scrollHeight = 0,
+  scrollPos = 0,
+  clonesHeight = 0,
+  i = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function getScrollPos () {
+  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function setScrollPos (pos) {
+  context.scrollTop = pos;
 }
 
-function showSlides(n) {
-  var i;
-  if  (n > $('.slides').length) {slideIndex = 1}
-  if (n < 1) {slideIndex = $('.slides').length}
-  for (i = 0; i < $('.slides').length; i++){
-    $('.slides')[i].style.display = "none";
+function getClonesHeight () {
+  clonesHeight = 0;
+
+  for (i = 0; i < clones.length; i += 1) {
+    clonesHeight = clonesHeight + clones[i].offsetHeight;
   }
-  $('.slides')[slideIndex-1].style.display = "block";
+
+  return clonesHeight;
 }
+
+function reCalc () {
+  scrollPos = getScrollPos();
+  scrollHeight = context.scrollHeight;
+  clonesHeight = getClonesHeight();
+
+  if (scrollPos <= 0) {
+    setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
+  }
+}
+
+function scrollUpdate () {
+  if (!disableScroll) {
+    scrollPos = getScrollPos();
+
+    if (clonesHeight + scrollPos >= scrollHeight) {
+      // Scroll to the top when you’ve reached the bottom
+      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+      disableScroll = true;
+    } else if (scrollPos <= 0) {
+      // Scroll to the bottom when you reach the top
+      setScrollPos(scrollHeight - clonesHeight);
+      disableScroll = true;
+    }
+  }
+
+  if (disableScroll) {
+    // Disable scroll-jumping for a short time to avoid flickering
+    window.setTimeout(function () {
+      disableScroll = false;
+    }, 40);
+  }
+}
+
+function init () {
+  reCalc();
+  
+  context.addEventListener('scroll', function () {
+    window.requestAnimationFrame(scrollUpdate);
+  }, false);
+
+  window.addEventListener('resize', function () {
+    window.requestAnimationFrame(reCalc);
+  }, false);
+}
+
+if (document.readyState !== 'loading') {
+  init()
+} else {
+  doc.addEventListener('DOMContentLoaded', init, false)
+}
+
+// let slideIndex = 1;
+// showSlides(slideIndex)
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//  showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+ //  if  (n > $('.slides').length) {slideIndex = 1}
+// if (n < 1) {slideIndex = $('.slides').length}
+ // for (i = 0; i < $('.slides').length; i++){
+  //  $('.slides')[i].style.display = "none";
+ // }
+//  $('.slides')[slideIndex-1].style.display = "block";
+// }
