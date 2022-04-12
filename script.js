061e8633c8519ee87ca03b5c3e0dbeaeaf5b734a
ritazhan <zhan.rita.info@gmail.com>
2022-04-11 21:58:26 -0400

    Worked on JS for project1 .horizontal to create increasing div when .horizontal reaches center of screen

--- script.js
+++ script.js
@@ -26,124 +26,175 @@ $(".horizontal").css({
 // let imgcount = $(".filmstrip img").length; 
 
 $(".filmstrip").css({
-  'width': ((($(".filmstrip-img").width() +16) * 3) + 32 +'px')
+  'width': ((($(".filmstrip-img").width() +16) * 3) + 20 +'px')
 });
 
+// window.onscroll = function(e) {
+//   if(window.scrollY) {
+//       if(window.pageYOffset > 50) {
+//           $(".horizontal").style.height = (600 + 'px');
+//       } else {
+//           $(".horizontal").style.height = (200 + 'px');
+//       }
+//   }
+// }
 
 
-var doc = window.document,
-  context = doc.querySelector('.js-loop'),
-  clones = context.querySelectorAll('.is-clone'),
-  disableScroll = false,
-  scrollHeight = 0,
-  scrollPos = 0,
-  clonesHeight = 0,
-  i = 0;
+// let windowCenter = $(window).height() / 2;
 
-function getScrollPos () {
-  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
-}
+// if($('.horizontal').offset().top() / 2 <= windowCenter){
+//   $('.horizontal').fadeIn();
+// }  
+// else if($('.horizontal').offset().top() / 2 >= windowCenter){
+//   $('.horizontal').fadeOut();
+// } 
 
-function setScrollPos (pos) {
-  context.scrollTop = pos;
-}
 
-function getClonesHeight () {
-  clonesHeight = 0;
 
-  for (i = 0; i < clones.length; i += 1) {
-    clonesHeight = clonesHeight + clones[i].offsetHeight;
+$(".content").scroll(function() {
+  increaseSize()
+});
+
+function increaseSize () {
+  console.log("existing");
+  let screenCenter = $(window).scrollTop() + $(window).height() / 2;
+
+  if ($(".horizontal").offset() >= screenCenter) {
+    $(".horizontal").style.height = (600 + 'px');
+    console.log( "working!" );
   }
-
-  return clonesHeight;
-}
-
-function reCalc () {
-  scrollPos = getScrollPos();
-  scrollHeight = context.scrollHeight;
-  clonesHeight = getClonesHeight();
-
-  if (scrollPos <= 0) {
-    setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
-  }
-}
-
-function scrollUpdate () {
-  if (!disableScroll) {
-    scrollPos = getScrollPos();
-
-    if (clonesHeight + scrollPos >= scrollHeight) {
-      // Scroll to the top when you’ve reached the bottom
-      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
-      disableScroll = true;
-    } else if (scrollPos <= 0) {
-      // Scroll to the bottom when you reach the top
-      setScrollPos(scrollHeight - clonesHeight);
-      disableScroll = true;
-    }
-  }
-
-  if (disableScroll) {
-    // Disable scroll-jumping for a short time to avoid flickering
-    window.setTimeout(function () {
-      disableScroll = false;
-    }, 40);
+  else if ($(".horizontal").offset() <= screenCenter) {
+    $(".horizontal").style.height = (200 + 'px');
+    console.log( "working2!" );
   }
 }
 
-function init () {
-  reCalc();
+
+// $(window).on('scroll', function () {
+//   var scrollTop = $(window).scrollTop();
+//   if (scrollTop > 50) {
+//       $('.filmstrip-img').stop().animate({height: "30px"},200);
+//   }
+//   else {
+//        $('.filmstrip-img').stop().animate({height: "50px"},200);   
+//   }
+// });
+
+
+
+// var doc = window.document,
+//   context = doc.querySelector('.js-loop'),
+//   clones = context.querySelectorAll('.is-clone'),
+//   disableScroll = false,
+//   scrollHeight = 0,
+//   scrollPos = 0,
+//   clonesHeight = 0,
+//   i = 0;
+
+// function getScrollPos () {
+//   return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+// }
+
+// function setScrollPos (pos) {
+//   context.scrollTop = pos;
+// }
+
+// function getClonesHeight () {
+//   clonesHeight = 0;
+
+//   for (i = 0; i < clones.length; i += 1) {
+//     clonesHeight = clonesHeight + clones[i].offsetHeight;
+//   }
+
+//   return clonesHeight;
+// }
+
+// function reCalc () {
+//   scrollPos = getScrollPos();
+//   scrollHeight = context.scrollHeight;
+//   clonesHeight = getClonesHeight();
+
+//   if (scrollPos <= 0) {
+//     setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
+//   }
+// }
+
+// function scrollUpdate () {
+//   if (!disableScroll) {
+//     scrollPos = getScrollPos();
+
+//     if (clonesHeight + scrollPos >= scrollHeight) {
+//       // Scroll to the top when you’ve reached the bottom
+//       setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+//       disableScroll = true;
+//     } else if (scrollPos <= 0) {
+//       // Scroll to the bottom when you reach the top
+//       setScrollPos(scrollHeight - clonesHeight);
+//       disableScroll = true;
+//     }
+//   }
+
+//   if (disableScroll) {
+//     // Disable scroll-jumping for a short time to avoid flickering
+//     window.setTimeout(function () {
+//       disableScroll = false;
+//     }, 40);
+//   }
+// }
+
+// function init () {
+//   reCalc();
   
-  context.addEventListener('scroll', function () {
-    window.requestAnimationFrame(scrollUpdate);
-  }, false);
+//   context.addEventListener('scroll', function () {
+//     window.requestAnimationFrame(scrollUpdate);
+//   }, false);
 
-  window.addEventListener('resize', function () {
-    window.requestAnimationFrame(reCalc);
-  }, false);
-}
+//   window.addEventListener('resize', function () {
+//     window.requestAnimationFrame(reCalc);
+//   }, false);
+// }
 
-if (document.readyState !== 'loading') {
-  init()
-} else {
-  doc.addEventListener('DOMContentLoaded', init, false)
-}
-
-
+// if (document.readyState !== 'loading') {
+//   init()
+// } else {
+//   doc.addEventListener('DOMContentLoaded', init, false)
+// }
 
 
-/*horizontal scroll*/
-var page = document.getElementById('horizontal-loop');
-var last_loop = page.getElementsByClassName('loop');
-last_loop = last_loop[last_loop.length-1];
-var dummy_x = null;
 
-window.onscroll = function () {
-  // Horizontal Scroll.
-  var y = document.body.getBoundingClientRect().top;
-  page.scrollLeft = -y;
+
+// /*horizontal scroll*/
+// var page = document.getElementById('horizontal-loop');
+// var last_loop = page.getElementsByClassName('loop');
+// last_loop = last_loop[last_loop.length-1];
+// var dummy_x = null;
+
+// window.onscroll = function () {
+//   // Horizontal Scroll.
+//   var y = document.body.getBoundingClientRect().top;
+//   page.scrollLeft = -y;
   
-  // Looping Scroll.
-  var diff = window.scrollY - dummy_x;
-  if (diff > 0) {
-    window.scrollTo(0, diff);
-  }
-  else if (window.scrollY == 0) {
-    window.scrollTo(0, dummy_x);
-  }
-}
-// Adjust the body height if the window resizes.
-window.onresize = resize;
-// Initial resize.
-resize();
+//   // Looping Scroll.
+//   var diff = window.scrollY - dummy_x;
+//   if (diff > 0) {
+//     window.scrollTo(0, diff);
+//   }
+//   else if (window.scrollY == 0) {
+//     window.scrollTo(0, dummy_x);
+//   }
+// }
+// // Adjust the body height if the window resizes.
+// window.onresize = resize;
+// // Initial resize.
+// resize();
 
-// Reset window-based vars
-function resize() {
-  var w = page.scrollWidth-window.innerWidth+window.innerHeight;
-  document.body.style.height = w + 'px';
+// // Reset window-based vars
+// function resize() {
+//   var w = page.scrollWidth-window.innerWidth+window.innerHeight;
+//   document.body.style.height = w + 'px';
   
-  dummy_x = last_loop.getBoundingClientRect().left+window.scrollY;
-}
+//   dummy_x = last_loop.getBoundingClientRect().left+window.scrollY;
+// }
 
 
 
