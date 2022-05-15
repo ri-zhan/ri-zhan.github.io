764c271e5ceecce9d49cfe5835daa18b338326b4
ritazhan <zhan.rita.info@gmail.com>
2022-05-15 12:13:32 -0700

    Cleaned up CSS code. Need to update play and project1 HTML

--- js/script.js
+++ js/script.js
@@ -8,6 +8,40 @@ $(document).mousemove(function(e){
   $(".follower-center").css({left: e.pageX, top:e.pageY});
 });
 
+//////////////////////// inner container hover //////////////////
+$('.inner-border-top-left').hover(
+  function(){
+    console.log('hover working')
+    $('.inner-border-top-left-name').addClass('visible')
+  }, function(){
+    $('.inner-border-top-left-name').removeClass('visible')
+  }
+)
+
+$('.inner-border-top-right').hover(
+  function(){
+    $('.inner-border-top-right-name').addClass('visible')
+  }, function(){
+    $('.inner-border-top-right-name').removeClass('visible')
+  }
+)
+
+$('.inner-border-bottom-left').hover(
+  function(){
+    $('.inner-border-bottom-left-name').addClass('visible')
+  }, function(){
+    $('.inner-border-bottom-left-name').removeClass('visible')
+  }
+)
+
+$('.inner-border-bottom-right').hover(
+  function(){
+    $('.inner-border-bottom-right-name').addClass('visible')
+  }, function(){
+    $('.inner-border-bottom-right-name').removeClass('visible')
+  }
+)
+
 
 ///////////////// .content size
 $(".content").css({
