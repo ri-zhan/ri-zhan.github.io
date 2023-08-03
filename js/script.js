271337487b2eac925a4ea8e3e549971c68b888aa
ritazhan <zhan.rita.info@gmail.com>
2023-08-03 02:27:20 -0700

    changed the horizontal scroll to a top nav section menu

--- js/script.js
+++ js/script.js
@@ -1448,3 +1448,25 @@ $('.focus-ring').scroll(function () {
 //       $('#left').scrollTop($(this).scrollTop());
 //   });
 // });
+
+
+// cache the element
+var $navBar = $('.container111');
+
+// find original navigation bar position
+var navPos = $navBar.offset().top;
+
+// on scroll
+$('.content').scroll(function() {
+
+    // get scroll position from top of the page
+    var scrollPos = $(this).scrollTop();
+
+    // check if scroll position is >= the nav position
+    if (scrollPos >= navPos) {
+        $navBar.addClass('fixed');
+    } else {
+        $navBar.removeClass('fixed');
+    }
+
+});
