d4dac05927e4bb51e20c59867373f7efeafc55cd
Ri Zhan <git@ri.works>
2025-08-18 14:23:18 -0400

    again

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     dir: {
