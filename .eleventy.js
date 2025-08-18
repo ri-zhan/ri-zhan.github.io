7686a66cbd190c3a9d62d3fb535e0686ea73184e
Ri Zhan <git@ri.works>
2025-08-18 13:41:32 -0400

    feat: githutb actions test

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     dir: {
